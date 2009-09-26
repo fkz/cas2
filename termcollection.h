a57b43249a0479d7512e2d3e4008331ff3eb374d
fabian <fabian@99334987-7fb6-4f9b-9453-cf9f8c1b9a5c>
2009-09-26 14:46:09 +0000

    Term-Collection-Klasse hinzugefügt, die von std::multimap abgeleitet ist; wie std::set funktioniert und das Handling von Termsammlungen vereinfachen soll
    git-svn-id: svn://localhost/trunk/c++/mathematics/cas2@236 99334987-7fb6-4f9b-9453-cf9f8c1b9a5c

---
+++ termcollection.h
@@ -0,0 +1,39 @@
+/*
+    <one line to give the program's name and a brief idea of what it does.>
+    Copyright (C) <year>  <name of author>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+*/
+
+#ifndef CAS_TERMCOLLECTION_H
+#define CAS_TERMCOLLECTION_H
+#include <map>
+#include "hash.h"
+
+namespace CAS {
+class Term;
+
+class TermCollection: public std::multimap< Hash, Term * >
+{
+  public:
+    bool push_back (Term *t);
+    const_iterator find (const CAS::Term* t) const;
+    iterator find (const CAS::Term *t);
+};
+
+}
+
+#endif // CAS_TERMCOLLECTION_H
